717c83fd9e82bbe9b132febcb6e4c729306a0e13
AngelDPS <adps@gmail.com>
2023-07-13 12:06:16 -0400

    Pre-filtro para el evento

--- init.py
+++ init.py
@@ -33,9 +33,28 @@ def lambda_handler(event: List[dict], context: Any) -> List[Dict[str, str]]:
         environ["NOMBRE_COMPANIA"] = "generico2022"
         environ["AWS_REGION"] = "us-east-2"
 
+    filtro_campos = [
+        "prec_vta1", "prec_vta2", "prec_vta3", "PK", "SK", "habilitado",
+        "art_des", "codigoCompania", "codigoTienda", "co_art", "co_lin",
+        "stock_act", "stock_com", "codigo_barra", "referencia", "marca",
+        "imagen_url", "modelo"
+    ]
+
+    if event[0].get("eventName") == "INSERT":
+        for key in filtro_campos:
+            if key not in event[0]["dynamodb"]["NewImage"]:
+                raise ValueError("El evento no contiene el campo " + key)
+    elif event[0].get("eventName") == "MODIFY":
+        for key in filtro_campos:
+            if (key not in event[0]["dynamodb"]["NewImage"]
+                    or key not in event[0]["dynamodb"]["OldImage"]):
+                raise ValueError("El evento no contiene el campo " + key)
+    else:
+        raise ValueError("El evento no es válido")
+
     handler_mapping = {
-                'articulos': ProductoHandler,
-                'lineas': ColeccionHandler
-                # 'tiendas': SucursalHandler
-            }
+        'articulos': ProductoHandler,
+        'lineas': ColeccionHandler
+        # 'tiendas': SucursalHandler
+    }
     return procesar_todo('shopify', event, handler_mapping)
